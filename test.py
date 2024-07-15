055099f6d043982b0042ce86892950cc757b4f4c
lisheld <lsheldon@mit.edu>
2024-07-15 13:19:13 -0400

    fixed scores and league updates

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from vars import base_params,base_url
 from newhelpers import get_endpoint
-from newfuncs import get_scores
-print(get_scores('Spain Engand Uefa Euros 2024'))
+from newfuncs import *
+print(get_prediciton('mlb h2h'))
